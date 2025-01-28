d76084b4b877db8f7189134028771670fea0512c
mdtanvirislamrakib <mdtanvirislamrakib7@gmail.com>
2025-01-28 14:55:54 +0600

    styling navbar section and header section

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [".html"],
+    theme: {
+      extend: {
+        colors: {
+            clifford: '#da373d',
+            'bike-primary': '#E76F51',
+            'bike-primary-bg': 'rgba(231, 111, 81, 0.1)',
+          },
+      },
+    },
+    plugins: [],
+}
