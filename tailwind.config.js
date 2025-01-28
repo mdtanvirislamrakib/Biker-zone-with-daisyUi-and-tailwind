1d7021c9238da3b0ba335cd91bf90fc24abad476
mdtanvirislamrakib <mdtanvirislamrakib7@gmail.com>
2025-01-28 21:25:20 +0600

    footer section added and styling this

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
             clifford: '#da373d',
             'bike-primary': '#E76F51',
             'bike-primary-bg': 'rgba(231, 111, 81, 0.1)',
-            'client-color' : 'rgb(35, 166, 240)'
+            'client-color' : 'rgb(35, 166, 240)',
+            'footer-bg' : 'rgb(11, 13, 23)',
           },
       },
     },
